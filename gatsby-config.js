ee07d19dff428816a5822dd543c39e08b26baa2f
Jose Angel Hidalgo Rosa <joseangel1196@hotmail.com>
2020-09-08 19:18:11 -0400

    Converting project to typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+    },
+  ],
 }
