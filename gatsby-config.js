17c35b323a2fa84972fc44bf280797d6f87b5221
Jose Angel Hidalgo Rosa <joseangel1196@hotmail.com>
2020-09-08 21:12:31 -0400

    Adding Tailwind to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-typescript`,
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+    },
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: ["src/css/index.css"],
+      },
+    },
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Home = () => {
-  return <h1>Hello World</h1>
+  return <h1 className="text-center">Hello World</h1>
 }
 
 export default Home
